735fc0190aa0c3091441e4e8410a4a830cc5403d
hannasabrynna <hannasabrynna135@gmail.com>
2025-07-29 13:03:51 -0300

    Ajustando tamanho da tela do VLibras

--- lib/app/views/widgets/VLibrasOverlay.dart
+++ lib/app/views/widgets/VLibrasOverlay.dart
@@ -10,27 +10,40 @@ class VLibrasOverlay extends StatefulWidget {
 
 class _VLibrasOverlayState extends State<VLibrasOverlay> {
   late final WebViewController _controller;
+  bool _expanded = false;
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'VLibrasChannel',
+        onMessageReceived: (message) {
+          setState(() {
+            _expanded = message.message == 'expand';
+            if (message.message == 'collapse') {
+              _expanded = false;
+            }
+          });
+        },
+      )
       ..loadFlutterAsset('assets/vlibras/vlibras_widget.html');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 0,
+    final screenHeight = MediaQuery.of(context).size.height;
+    final buttonHeight = _expanded ? 350 : 60;
+    final top = (screenHeight - buttonHeight) / 2;
+
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+      top: top,
       right: 0,
-      left: 0,
-      top: 0,
-      child: IgnorePointer(
-        ignoring:
-            true, // Deixa a WebView "transparente" para toques, exceto no VLibras
-        child: WebViewWidget(controller: _controller),
-      ),
+      width: _expanded ? 200 : 60,
+      height: buttonHeight.toDouble(),
+      child: WebViewWidget(controller: _controller),
     );
   }
 }
